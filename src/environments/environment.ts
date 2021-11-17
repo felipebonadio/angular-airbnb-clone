ed894817f269123733407624fba1763034805917
Felipe Chagas Bonadio <felipe.bonadio@etec.sp.gov.br>
2021-11-17 15:40:37 -0300

    envorinments configs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   REST_API_URL: 'https://spring-airbnb-startdb.herokuapp.com/api/'
 };
 
