55b2475e8fb8ba7244ad5b2b72f7fcc018a3afd4
Felipe Chagas Bonadio <felipe.bonadio@etec.sp.gov.br>
2021-11-17 15:35:31 -0300

    Heroku config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   REST_API_URL: 'https://spring-airbnb-startdb.herokuapp.com/api/'
 };
 
