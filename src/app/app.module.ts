d1a0686e5261b2cdc41151f30515a72675fc7058
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2021-11-29 10:58:26 -0300

    updated css and hmtl of host, guest and room

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { HostDetailComponent } from './host/host-detail/host-detail.component';
 import { GuestDetailComponent } from './guest/guest-detail/guest-detail.component';
-import { GuestAuthComponent } from './auth/guest-auth/guest-auth.component';
+
 
 
 
@@ -42,7 +42,6 @@ import { GuestAuthComponent } from './auth/guest-auth/guest-auth.component';
     ModalComponent,
     HostDetailComponent,
     GuestDetailComponent,
-    GuestAuthComponent,
   ],
   imports: [
     BrowserModule,
